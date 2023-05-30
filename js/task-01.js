108d03a7523b80ce3b5458bbb509a81e70a0a924
KostiZin <pancostian@gmail.com>
2023-05-29 21:29:02 -0400

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,16 @@
+const list = document.querySelectorAll(`li.item`)
+
+console.log(`Number of categories:`, list.length);
+
+console.log(list);
+
+const category = document.querySelectorAll(`h2`)
+
+console.log(category);
+
+// let categoryNumber = list.forEach(number) => {categoryNumber += number};
+
+
+
+
+// console.log(categoryNumber);
