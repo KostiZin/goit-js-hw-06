be7d6973d12f5d0e64675a11b55610c106e5e4a1
KostiZin <pancostian@gmail.com>
2023-06-01 19:14:47 -0400

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -4,26 +4,27 @@ console.log(`Number of categories:`, list.length);
 
 const category = document.querySelectorAll(`h2`);
 
-console.log(category);
+// console.log(category);
 
-category.forEach((h2) => 
-{console.log((`Category: ` + h2.textContent + `; Elements: ` + h2.nextElementSibling
-.children.length))});
+category.forEach((h2) => {
+  console.log(
+    `Category: ` +
+      h2.textContent +
+      `; \nElements: ` +
+      h2.nextElementSibling.children.length
+  );
+});
 
-// list.forEach((tyb) => console.log(`Elements:`, tyb.lastElementChild.children.length));
-
-// Prosto querySelector ==========================
+// Just querySelector (example for myself) ==========================
 
 // const list = document.querySelector(`#categories`);
-
 
 // console.log(`Number of categories:`, list.children.length);
 
 // const category = document.querySelector(`.item`);
 
-
 // console.log(`Category:`, category.firstElementChild.textContent);
 
 // console.log(`Elements:`, category.lastElementChild.children.length);
 
-// Prosto querySelector ==========================
+// Just querySelector (example for myself) ==========================
