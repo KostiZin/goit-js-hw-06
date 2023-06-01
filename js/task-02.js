3518ad45ca8da5ac4a751655c2518da63cb045f7
KostiZin <pancostian@gmail.com>
2023-06-01 19:14:52 -0400

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,24 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsEl = document.querySelector(`#ingredients`);
+
+const elements = ingredients.map((ingredientsName) => {
+  const liEl = document.createElement(`li`);
+  liEl.textContent = ingredientsName;
+  liEl.classList.add(`item`);
+
+  return liEl;
+});
+
+console.log(elements);
+
+ingredientsEl.append(...elements);
+
+console.log(ingredientsEl);
