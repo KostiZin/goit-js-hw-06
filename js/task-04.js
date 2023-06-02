0a3222d6d3b848af7ef233c5a8b3f3fdc096e4be
KostiZin <pancostian@gmail.com>
2023-06-02 16:42:33 -0400

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,28 @@
+const counterEl = document.querySelector(`#counter`);
+
+// console.dir(counterEl);
+
+const counterValue = document.querySelector(`#value`);
+
+// console.log(counterValue);
+
+const btnDecrease = counterEl.firstElementChild;
+// console.log(btnDecrease);
+
+const btnIncrease = counterEl.lastElementChild;
+
+// console.log(btnIncrease);
+
+btnDecrease.addEventListener(`click`, onClickDecrease);
+
+function onClickDecrease() {
+  counterValue.textContent -= 1;
+  //   console.log(evt.currentTarget);
+}
+
+btnIncrease.addEventListener(`click`, onClickIncrease);
+
+function onClickIncrease() {
+  counterValue.textContent -= -1;
+  //   console.log(evt.currentTarget);
+}
